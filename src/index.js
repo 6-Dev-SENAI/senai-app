15678903224fe0ec3391411c76aab32ccfe22921
Gustavo-Apolonio <gustta.apolonio@gmail.com>
2021-09-15 18:28:32 -0300

    Projeto - OK

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Home from './components/homePage'
+import Header from './components/header'
+import Footer from './components/footer'
+
+import './assets/styles/global.css'
+
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      Hello World!
+    <div className="container container-global">
+      <Header />
+      <Home />
+      <Footer />
     </div>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
